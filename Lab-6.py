857a233b469309f4298f55a66d6819126c38fbdf
Aaron Tran <dahpaladin123@gmail.com>
2023-10-24 19:15:43 -0400

    push from aaron tran

--- Lab-6.py
+++ Lab-6.py
@@ -4,14 +4,16 @@ def main():
     opas = ''
     while True:
         print("1. Encode\n2. Decode\n3. Quit")
-        inp = int(input())
+        inp = int(input('Please enter an option: '))
+        
         #Comment
         if inp == 1:
             pas = input("Please enter your password to encode: ")
             opas = encode(pas)
             print("Your password has been encoded and saved!")
         if inp == 2:
-            pass
+            opas2 = decode(opas)
+            print('The encoded password is', opas, ', and the original password is', opas2)
         if inp == 3:
             break
 
@@ -23,6 +25,15 @@ def encode(num):
         numm[i] = int(num[i]) + 3
         new += str(numm[i])
     return new
+
+def decode(opas):
+    temp_string = ''        
+    for i in opas:
+        i = int(i)
+        i -= 3
+        temp_string += str(i)
+    return temp_string
+
 
 
 if __name__ == "__main__":
